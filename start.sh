9b5f8c2c32d5cae403bec192b7cad4439926f415
NJORO <karugunjoroge@gmail.com>
2019-08-06 15:40:12 +0300

    depl

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='trap'
-# export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://njoroge:1234@localhost/njoroge'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://njoroge:njoro1234@localhost/njoroge'
 export MAIL_USERNAME='karugunjoroge@gmail.com'
 export MAIL_PASSWORD='njoro1234'
 # export DATABASE_URL = 'mysql+mysqldb://njoroge:1234@localhost/njoroge.mysql'
