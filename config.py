5153366912b7af01195bafb9b167ccc1716bd834
NJORO <karugunjoroge@gmail.com>
2019-08-06 13:05:17 +0300

    dEPLOYMENT

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ class Config:
 
 
 
-class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")'
+class ProdConfig(Config): 
+      SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
 
 class DevConfig(Config):
